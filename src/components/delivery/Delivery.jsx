e8ad41d290d7e216661b7634b89776a074ac1b01
Artem <fdggfh@gmail.com>
2024-02-08 10:05:58 +0300

    MainButton in dilevery

--- src/components/delivery/Delivery.jsx
+++ src/components/delivery/Delivery.jsx
@@ -1,15 +1,17 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { userContext } from '../../App';
 import s from './Delivery.module.css';
 import pita from '../../fonts/pita.png';
 import burger from '../../fonts/burger.png';
 import shawarma from '../../fonts/shawarma.png';
+import { useTelegram } from '../../hooks/useTelegram';
 
 const Delivery = () => {    
     const { order } = useContext(userContext);
     const [ userName, setUserName ] = useState();
     const [ phone, setPhone ] = useState();
     const [ adress, setAdress ] = useState();
+    const {tg} = useTelegram();
 
     const onChangeUserName = (e) => {
         setUserName(e.target.value)
@@ -30,7 +32,7 @@ const Delivery = () => {
         tg.MainButton.setParams({
             text: `Заказать ${totalCost}р`,
         });
-    }, [order, tg.MainButton]);
+    }, [userName, phone, adress]);
 
     useEffect(() => {
         // Показываем или скрываем кнопку в зависимости от наличия товаров в заказе
